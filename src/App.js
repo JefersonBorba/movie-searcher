f787bd5ec391650fba7029337898ac53a7bdba64
Jeferson <jefersonborba@protonmail.com>
2021-01-04 00:10:14 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react"
 import './App.css';
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [currentPage, setCurrentPage] = useState(1)
+  useEffect(() => {
+    axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=9999516024fcedf9b329414356aebb69&page=${currentPage}`)
+    .then((res) => setData(res.data))
+  },[currentPage],)
+  const [data, setData] = useState()
+  console.log(data)
+  const nextPage = () => {
+    setCurrentPage(currentPage + 1);
+  }
+  const previousPage = () => {
+    setCurrentPage(currentPage - 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data && data.results.map((data) => (
+        <div className="card" style={{display: "flex", flexDirection: "column", alignItems: "center", width: "300px"}}>
+          <img src={`https://image.tmdb.org/t/p/w200/${data.poster_path}`} alt="poster"/>
+          <h3>{data.original_title}</h3>
+          <p>Release Date: {data.release_date}</p>
+          <h4>Overview:</h4>
+          <p>{data.overview}</p>
+        </div>
+      ))
+      }
+      {currentPage !== 1 && <button onClick={previousPage}>Previous Page</button>}
+      <button onClick={nextPage}>Next Page</button>
     </div>
   );
 }
